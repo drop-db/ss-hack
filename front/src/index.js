0c039a4c7f0373abf55ba821a47e0daf0264703c
nikita <xiagn17@gmail.com>
2019-09-28 16:03:34 +0300

    navigato

--- front/src/index.js
+++ front/src/index.js
@@ -4,6 +4,37 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+(function() {
+    if(!navigator.mediaDevices) navigator.mediaDevices = {};
+    if(!navigator.mediaDevices.getUserMedia) {
+        var getUserMedia = (
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia ||
+            navigator.getUserMedia
+        );
+
+        if(getUserMedia) {
+            navigator.mediaDevices.getUserMedia = function(constraints) {
+                return new Promise(function(resolve, reject) {
+                    getUserMedia(constraints,
+                        function(stream) { resolve(stream); },
+                        function(error) { reject(error); }
+                    )
+                });
+            };
+        }
+        else {
+            navigator.mediaDevices.getUserMedia = function() {
+                return new Promise(function(resolve, reject) {
+                    reject('getUserMedia is not supported in this browser.')
+                });
+            };
+        }
+    }
+})();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
