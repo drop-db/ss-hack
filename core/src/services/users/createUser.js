e56d3bea31df99f9908363a56c366ba83c5186f1
Semyon <yohimik@gmail.com>
2019-09-29 00:09:50 +0300

    added system chat and read messages

--- core/src/services/users/createUser.js
+++ core/src/services/users/createUser.js
@@ -1,4 +1,6 @@
 const User = require('../../models/user.model');
+const Chat = require('../../models/chat.model');
+const ChatMessage = require('../../models/chatMessage.model');
 const { noEmailRegister } = require('../../config/vars');
 const { generateConfirmToken } = require('./createConfirmToken');
 const { sendConfirmEmail } = require('./sendConfirmEmail');
@@ -45,7 +47,7 @@ async function createUser(data, session) {
         hasChild,
     } = data;
 
-    const user = await User.create({
+    const [user] = await User.create([{
         firstName,
         lastName,
         email,
@@ -69,7 +71,22 @@ async function createUser(data, session) {
         hasChild,
         sex,
         status: noEmailRegister ? User.USER_STATUS.ACTIVE : User.USER_STATUS.NOT_CONFIRMED,
-    });
+    }], { session });
+
+    const [systemChat] = await Chat.create([{
+        name: 'Уведомления',
+        system: user,
+    }]);
+    const [firstMessage] = await ChatMessage.create([{
+        message: `Благодарим за регистрацию ${firstName} ${lastName}! Этот канал предназначен для рассылки оповещений на платформе. В ближайшее время с вами свяжется ваш куратор и даст остальную информацию. Хорошего дня, ${firstName}!`,
+        chat: systemChat,
+    }], { session });
+    systemChat.messages.push(firstMessage);
+    user.chats.push(systemChat);
+    await Promise.all([
+        systemChat.save(),
+        user.save(),
+    ]);
 
     if (!noEmailRegister) {
         const confirmToken = await generateConfirmToken(user, session);
