c9a1e4d14524e6f023e9c3e3737efe72a16f5666
nikita <xiagn17@gmail.com>
2019-09-28 03:34:23 +0300

    socket token init

--- front/src/modules/SocketClient.js
+++ front/src/modules/SocketClient.js
@@ -2,9 +2,11 @@ import io from 'socket.io-client';
 
 export default class SocketClient {
     constructor({events, url}) {
-        this.socket = io(url, {transports: ['websocket']});
+        const user = JSON.parse(localStorage.getItem('sunCityUser'));
+
+        this.socket = io(`${url}?accessToken=${user.accessToken}`, {transports: ['websocket']});
         this.socket.on('connect', () => {
-            console.log('socker connected');
+            console.log('socket connected');
         });
             events.forEach(({key, handler}) => {
             this.socket.on(key, handler);
